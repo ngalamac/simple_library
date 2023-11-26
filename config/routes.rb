bf5a97e9c0da423f53ef63cd424eface26686fbf
NGALA MAC <macngala4@gmail.com>
2023-11-26 12:10:22 +0100

    Complete exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get 'book/list'
+  get 'book/new'
+  post 'book/create'
+  patch 'book/update'
+  get 'book/list'
+  get 'book/show'
+  get 'book/edit'
+  get 'book/delete'
+  get 'book/update'
+  get 'book/show_subjects'
+
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  
 end
